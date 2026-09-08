076c854569cb092d5c341fedc3c246b437f0cedd
屁 <75117205+pixingzoudaiyuexing@users.noreply.github.com>
2026-09-08 19:44:22 +0800

    feat: add linode cli commands scaffold

--- cmd/linode-tool/main.go
+++ cmd/linode-tool/main.go
@@ -1,8 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/pixingzoudaiyuexing/linode-tool/internal/linode"
+)
 
 func main() {
-	fmt.Println("linode-tool MVP")
-	fmt.Println("TODO: create/list/delete Linode instances")
+	if len(os.Args) < 2 {
+		usage()
+		return
+	}
+
+	client, err := linode.NewClient()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	switch os.Args[1] {
+	case "regions":
+		linode.PrintRegions(client)
+	case "list":
+		linode.PrintInstances(client)
+	case "create":
+		linode.CreateInteractive(client)
+	case "delete":
+		linode.DeleteInteractive(client)
+	default:
+		usage()
+	}
+}
+
+func usage() {
+	fmt.Println("linode-tool")
+	fmt.Println("")
+	fmt.Println("Commands:")
+	fmt.Println("  regions   show available regions")
+	fmt.Println("  create    create Debian 12 Nanode instance")
+	fmt.Println("  list      list instances")
+	fmt.Println("  delete    delete instance")
 }
